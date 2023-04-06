49b363d5337804e48b277bef4ba6835ac804c0b3
Marian <marianileana95@gmail.com>
2023-04-06 17:30:46 +0300

    lab1 ch2

--- src/main/kotlin/lab1/ch2.kt
+++ src/main/kotlin/lab1/ch2.kt
@@ -1,2 +1,20 @@
 package lab1
 
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    print("Enter your name: ")
+    val name = scanner.nextLine()
+    print("Enter your age: ")
+    val age = scanner.nextInt()
+
+
+    // Add logic to print greeting based on age
+    val greeting = when {
+        age < 18 -> "You're still young, $name!"
+        age in 18..65 -> "You're in your prime, $name!"
+        else -> "You're a wise elder, $name!"
+    }
+    println(greeting)
+}
